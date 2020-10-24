9982ea0bbba8f66ff29af1b05c6afa5cce2f45b8
alimesut <alimesut21@gmail.com>
2020-10-24 22:37:35 +0300

    rest_to_postgre.py code updated.

--- rest_in_python/rest_to_postgre.py
+++ rest_in_python/rest_to_postgre.py
@@ -1,11 +1,11 @@
 # Import libraries
-import requests #http request library
-import psycopg2 #postgre connection library
-import secrets #project secrets file
+import requests  # http request library
+import psycopg2  # postgre connection library
+import secrets  # project secrets file
 
 # Connecting to PostgreDB
-connection = psycopg2.connect(user = secrets.pg_user, password = secrets.pg_password,
-                              host = "127.0.0.1", port = "5432", database = "postgres")
+connection = psycopg2.connect(user=secrets.pg_user, password=secrets.pg_password,
+                              host="127.0.0.1", port="5432", database="postgres")
 cursor = connection.cursor()
 
 # API-Endpoint
@@ -20,10 +20,10 @@ data = r_get.json()
 # Write JSON data to PostgreDB table
 for p in data:
     userId = p['userId']
-    id = p['id']
+    albumId = p['id']
     title = p['title']
-    #print(str(userId) + '\t' + str(id) + '\t' + str(title))
-    cursor.execute("INSERT INTO typicode.albums (userid, id, title) VALUES ('%d','%d','%s')" %(userId,id,title))
+    # print(str(userId) + '\t' + str(id) + '\t' + str(title))
+    cursor.execute("INSERT INTO typicode.albums (userid, id, title) VALUES ('%d','%d','%s')" % (userId, albumId, title))
     connection.commit()
 cursor.close()
 connection.close()
